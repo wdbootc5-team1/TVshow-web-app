a92053d30ae601afbf8d04affd88183b1b9cee5b
sallysy8 <sallysy8@hotmail.com>
2019-08-14 11:16:36 -0700

    merge code and change port to 5200

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CurrentShowComponent } from './current-show/current-show.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrentShowComponent
   ],
   imports: [
     BrowserModule,
