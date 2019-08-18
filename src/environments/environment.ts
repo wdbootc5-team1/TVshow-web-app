640f696ee8e80fca71af41bd05c393dc4c50ddff
sallysy8 <sallysy8@hotmail.com>
2019-08-17 17:43:39 -0700

    Add API Key and gitignore the envrionment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appID: 'mnkVtLvDptrkEpjT0AekBdSq3mMRXT_Z',
+  baseUrl: 'http://',
 };
 
 /*
